1de1e4e8cbc22c0def700856b4c14a7b7a5c304c
Tanmay Bajaj <tanmaybajaj567@gmail.com>
2024-09-21 07:55:17 +0530

    integrate github auth for repos
    Signed-off-by: Tanmay Bajaj <tanmaybajaj567@gmail.com>

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HomePage from "@/pages/home";
 import DashboardPage from "@/pages/dashboard";
 import CodePage from "@/pages/code";
+import Callback from "./pages/callback";
 
 function App() {
   return (
@@ -10,6 +11,8 @@ function App() {
         <Route path="/" element={<HomePage />} />
         <Route path="/dashboard" element={<DashboardPage />} />
         <Route path="/code" element={<CodePage />} />
+        <Route path="/callback" element={<Callback />} />
+        <Route path="/repos" element={<div>Repos</div>} />
       </Routes>
     </Router>
   );
--- ui/src/pages/home.tsx
+++ ui/src/pages/home.tsx
@@ -3,7 +3,19 @@ import { useNavigate } from "react-router-dom";
 
 const HomePage = () => {
   const navigate = useNavigate();
-  return <Button onClick={() => navigate("/code")}>Go to editor</Button>;
+  return (
+    <div>
+      <Button onClick={() => navigate("/code")}>Go to editor</Button>
+      <Button
+        onClick={() =>
+          (window.location.href =
+            "https://github.com/login/oauth/authorize?client_id=Ov23liRv0ktKpaw29Jfw&scope=repo,user")
+        }
+      >
+        Sign in with GitHub
+      </Button>
+    </div>
+  );
 };
 
 export default HomePage;
